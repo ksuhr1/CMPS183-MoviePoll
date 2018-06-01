95b3e9c525bbdd6c24b5e9a64eef8e9129c5aa0f
anishatavva <Priyanka>
2018-05-31 20:10:15 -0700

    updated voting page layout and created vote button

--- static/js/default_vote.js
+++ static/js/default_vote.js
@@ -12,28 +12,11 @@ var app = function() {
         }
     };
     
-    self.getUberURL = function () {
-        var pp = {
-            client_id: "<CLIENT_ID>",
-            action: "setPickup",
+    //Write a function that will save the vote to the database
+    
 
-            
-            pickup: "my_location",
-            
-            dropoff: {
-                latitude: 37.802374,
-                longitude: -122.405818,
-                nickname: "Coit Tower",
-            },            
-
-            product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d",
-        }
-        self.vue.uberURL = "https://m.uber.com/ul/" + "?" + $.param(pp);
-    }
-
-
-
-    // ######################### Get polls
+    // ##############################################################
+    // Get polls
     function get_polls_url(start_idx, end_idx) {
         var pp = {
             start_idx: start_idx,
@@ -51,118 +34,24 @@ var app = function() {
         })
     };
 
-    function get_poll_url(poll_id) {
-        var pp = {
-            poll_id: poll_id
-        };
-        return poll_url + "?" + $.param(pp);
-    }
-
-    self.get_poll = function () {
-        $.getJSON(get_poll_url(poll_id), function (data) {
-            console.log(data);
-            self.vue.poll = data.poll;
-        })
-    };
-
-    self.get_more = function () {
-        var num_polls = self.vue.polls.length;
-        $.getJSON(get_polls_url(num_polls, num_polls + 4), function (data) {
-            self.vue.has_more = data.has_more;
-            self.extend(self.vue.polls, data.polls);
-        });
-    };
-
-
-
-
-    // ######################### Add polls
-    self.add_poll_button = function () {
-        // The button to add a track has been pressed.
-        if(self.vue.logged_in)
-          self.vue.is_adding_poll = !self.vue.is_adding_poll;
-    };
-
-    self.add_poll = function () {
-        // The submit button to add a track has been added.
-        $.post(add_poll_url,
+    // ##############################################################
+    // Get single poll based on poll id
+    self.get_poll = function (pollId) {
+        $.getJSON(poll_url,
             {
-                content: self.vue.form_content,
+                poll_id: poll_id,
             },
             function (data) {
-                $.web2py.enableElement($("#add_poll_submit"));
-                self.vue.polls.unshift(data.poll);
-                console.log(self.vue.polls.length);
-                // if polls length is greater than 4 has_more is true
-                if (self.vue.polls.length > 4) {
-                    self.vue.has_more = true;
-                }
-                self.vue.is_adding_poll = !self.vue.is_adding_poll;
-                self.vue.form_content = "";
-            });
-    };
-
-
-
-    // ######################### Edit polls
-    self.edit_poll_submit = function (poll_id) {
-        poll = self.vue.polls.find(poll => poll.id === poll_id);
-        poll.content = self.vue.edit_content;
-        $.post(edit_poll_url,
-            {
-                poll_content: self.vue.edit_content,
-                id: self.vue.edit_id
-            },
-            function (data) {
-                $.web2py.enableElement($("#edit_poll_submit"));
-                self.vue.editing = !self.vue.editing;
-            });
-    };
-
-    self.edit_poll = function(poll_id) {
-        self.vue.editing = !self.vue.editing;
-        self.vue.edit_id = poll_id;
-        poll = self.vue.polls.find(poll => poll.id === poll_id);
-        self.vue.edit_content = poll.content;
-    };
-
-    self.cancel_edit = function () {
-        self.vue.editing = !self.vue.editing;
-        self.vue.edit_id = 0;
-    };
-
-
-
-    // ######################### Delete polls
-    self.delete_poll = function(poll_id) {
-        $.post(del_poll_url,
-            {
-                poll_id: poll_id
-            },
-            function () {
-                pollIndex = self.vue.polls.findIndex(poll => poll.id === poll_id);
-                self.vue.polls.splice(pollIndex, 1);
+                console.log("here is your poll: ");
+                console.log(data);
+                self.vue.poll = data.poll;
+                self.vue.logged_in = data.logged_in;
             }
         )
     };
 
 
 
-    
-    // ######################### Toggle PUblic
-    self.toggle_public = function(poll_id) {
-        $.post(toggle_public_url,
-            {
-                poll_id: poll_id
-            },
-            function (data) {
-                poll = self.vue.polls.find(poll => poll.id === poll_id);
-                poll.is_public = data.poll.is_public;
-            }
-        )
-    }
-
-    // Complete as needed.
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
@@ -170,39 +59,20 @@ var app = function() {
         data: {
             poll: {},
             polls: [],
-            get_more: false,
-            has_more: false,
-            
-            logged_in: false,
-            
-            editing: false,
-            is_adding_poll: false,
 
-            form_content: null,
-            edit_content: null,
-            edit_id: 0,
 
             uberURL: null,
         },
         methods: {
-            get_more: self.get_more,
-            add_poll_button: self.add_poll_button,
-            add_poll: self.add_poll,
-            delete_poll: self.delete_poll,
-            edit_poll: self.edit_poll,
-            edit_poll_submit: self.edit_poll_submit,
-            cancel_edit: self.cancel_edit,
-            toggle_public: self.toggle_public,
-
-            getUberURL: self.getUberURL, 
+            get_polls: self.get_polls,
+            get_poll: self.get_poll,
         }
 
 
     });
 
     // self.get_polls();
-    self.get_poll();
-    self.getUberURL();
+    self.get_poll(poll_id);
     $("#vue-div").show();
     return self;
 };
